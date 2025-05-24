84a505dc979b98560a721a0be79c7efd136862c7
Jose Guevarra <jguevarra@verimatrix.com>
2025-05-24 00:51:31 +0000

    hw02

---
+++ jose_homework/hw02/start_mlflow_server.sh
@@ -0,0 +1,4 @@
+
+
+
+pipenv run mlflow server -h 0.0.0.0 -p 7003 --backend-store-uri sqlite:///mlflow.db --default-artifact-root model_reg
---
+++ jose_homework/hw02/start_mlflow_ui.sh
@@ -0,0 +1,5 @@
+
+
+
+
+pipenv run mlflow ui -p 7002 --backend-store-uri sqlite:///mlflow.db &
